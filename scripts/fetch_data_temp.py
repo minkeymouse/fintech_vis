322a1c07b51bbfb9396a5ecf2558c673169a8be4
hsrhee97 <rheeh97@gmail.com>
2024-08-20 15:19:28 +0900

    기상청 api 연결

---
+++ scripts/fetch_data_temp.py
@@ -0,0 +1,29 @@
+import requests
+import os
+
+# The URL provided by the API creator
+domain = 'https://apihub.kma.go.kr/api/typ01/url/sfc_norm1.php?'
+norm = 'norm=D&'
+tm = 'tmst=2021&'
+stn_id = 'stn=0&'
+option = 'MM1=5&DD1=1&MM2=5&DD2=2&'
+auth = 'authKey=-9IQCdUWT9ySEAnVFs_c4Q'
+
+api_url = domain + norm + tm + stn_id + option + auth
+
+# API 요청 보내기
+response = requests.get(api_url)
+
+# 응답 상태 확인
+if response.status_code == 200:
+    try:
+        # JSON 형식으로 데이터 파싱 시도
+        data = response.json()
+        # 데이터 출력 (예시)
+        print(data)
+    except requests.exceptions.JSONDecodeError:
+        # JSONDecodeError가 발생하면 응답 내용을 출력
+        print("Failed to decode JSON. Response content:")
+        print(response.text)
+else:
+    print(f"Error: {response.status_code}")
