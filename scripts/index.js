7b0c73c6a915b82b1805f144494dda973c1d5e2a
hsrhee97 <rheeh97@gmail.com>
2024-08-26 20:11:32 +0900

    이상기후 러브버그까지 완료

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,9 @@ import { startRainfallVisualization } from './visualization.js';
 import { startWork1Visualization } from './visualization.js';
 import { startTempAnomalyVisualization } from './visualization.js';
 import { startWBGTVisualization } from './visualization.js';
+import { drawSeoulMaps } from './visualization.js';
 
-// 첫 번째 섹션에 대한 관찰자 설정
+// general_one
 const observerOne = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -16,7 +17,7 @@ const observerOne = new IntersectionObserver((entries, observer) => {
 }, { threshold: 0.5 });
 observerOne.observe(document.querySelector('#general_one'));
 
-// 두번째 섹션에 대한 관찰자 설정
+// general_two
 const observerTwo = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -28,6 +29,44 @@ const observerTwo = new IntersectionObserver((entries, observer) => {
 }, { threshold: 0.5 });
 observerTwo.observe(document.querySelector('#general_two'));
 
+// general_three
+document.addEventListener('DOMContentLoaded', () => {
+    startTempAnomalyVisualization();
+});
+
+// -----------------------------------------------------
+
+// 열사병 1: 호우 태풍 폭염 그래프
+const observerWork1 = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            startWork1Visualization();  // 시각화 함수 호출
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.5 });
+observerWork1.observe(document.querySelector('#heatstroke_one'));
+
+// 열사병 2: 열스트레스 그래프
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            startWBGTVisualization();
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.5 });
+observer.observe(document.querySelector('#heatstroke_two'));
+
+// -----------------------------------------------------
+
+// 서울 러브버그 시각화
+// visualization.js에서 시각화 함수를 가져옵니다.
+document.addEventListener('DOMContentLoaded', function() {
+    drawSeoulMaps();
+});
+
+// -----------------------------------------------------
 
 // Rainfall network visualization observer
 const observerRainfall = new IntersectionObserver((entries, observer) => {
@@ -40,31 +79,3 @@ const observerRainfall = new IntersectionObserver((entries, observer) => {
 }, { threshold: 0.5 });
 observerRainfall.observe(document.querySelector('#three'));
 
-
-// // 열사병: 호우 태풍 폭염 그래프
-const observerWork1 = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            startWork1Visualization();  // 시각화 함수 호출
-            observer.unobserve(entry.target);
-        }
-    });
-}, { threshold: 0.5 });
-observerWork1.observe(document.querySelector('#heatstroke_one'));
-
-
-// general-one 페이지 로드 또는 특정 이벤트에서 시각화 함수 호출
-document.addEventListener('DOMContentLoaded', () => {
-    startTempAnomalyVisualization();
-});
-
-//열사병 Graph: 열스트레스 그래프
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            startWBGTVisualization();
-            observer.unobserve(entry.target);
-        }
-    });
-}, { threshold: 0.5 });
-observer.observe(document.querySelector('#heatstroke_two'));
